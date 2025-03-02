75b871884a15c6eafdd0c93206d54bf9356e78e9
Abdussamet KACI <abdussamet.kaci@d-teknoloji.com.tr>
2025-03-02 15:59:12 +0300

    Init app

---
+++ src/test/kotlin/org/demo/springbootcache/SpringCacheDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.demo.springbootcache
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringCacheDemoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
